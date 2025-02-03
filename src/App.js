6d5866b62ecedda8cc037f1ccd4c42d527836dbb
Adrian <adriansanduta@gmail.com>
2025-02-02 19:28:12 -0600

    1.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Home />} ></Route>
+        <Route path=":id"></Route>
+      </Routes>
     </div>
+    </Router>
   );
 }
 
