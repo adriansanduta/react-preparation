82382bb4c5accb12a4793a06bab4bde4ab15db8e
Adrian <adriansanduta@gmail.com>
2025-02-02 19:34:49 -0600

    1.2

---
+++ src/pages/Posts.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const Posts = () => {
+    const params = useParams();
+  return (
+    <div>
+      Posts
+    </div>
+  );
+}
+
+export default Posts;
