254dabc9f51662ea8e9ee63bd301620ce7db2242
Adrian <adriansanduta@gmail.com>
2025-02-08 20:26:33 -0600

    1.4

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,14 +1,44 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const Posts = () => {
     const {id} = useParams();
+    const [posts, setPosts] = useState([]);
 
-    useEffect(() => {}, []);
+   
+    useEffect( () => {
+        async function fetchPosts () {
+     const {data} = await axios.get("https://jsonplaceholder.typicode.com/posts?userId=${id}");
+     setPosts(data);
+    }
+    fetchPosts();
+   }, []);
   return (
-    <div>
-      {id}
+    <>
+  <div className="post__search">
+    <button>← Back</button>
+    <div className="post__search--container">
+      <label className="post__search--label">Search by Id</label>
+      <input
+        type="number"
+      />
+      <button>Enter</button>
     </div>
+  </div>
+  <div className="post">
+    <div className="post__title">post.title</div>
+    <p className="post__body">post.body</p>
+  </div>
+  <div className="post">
+    <div className="post__title">
+      <div className="post__title--skeleton"></div>
+    </div>
+    <div className="post__body">
+      <p className="post__body--skeleton"></p>
+    </div>
+  </div>
+</>
   );
 }
 
