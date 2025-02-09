f842e7ba23296703c3fa7b2f1d04dfdeee274064
Adrian <adriansanduta@gmail.com>
2025-02-08 21:51:16 -0600

    2.1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,25 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import {useNavigate} from 'react-router-dom';
 
 const Home = () => {
     const [users, setUsers] = useState([]);
+    const navigate = useNavigate();
 
-    async function fetchUsers() {
+    async function getUsers() {
         const {data} = await axios.get('https://jsonplaceholder.typicode.com/users');
         setUsers(data);
     }
+
+    useEffect(() => {
+        getUsers();
+    }, []);
     return (
         <div className="container">
   <div className="row">
     <div className="user-list">
         {users.map((user) => (
-         <div className="user">
+         <div className="user" key={user.id} onClick={() => navigate(`${user.id}`)} >
          <div className="user-card">
            <div className="user-card__container">
              <h3>{user.name}</h3>
