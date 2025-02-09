d2edaa3e0e6ec00cdb8f64b989bf8ee60f968a01
Adrian <adriansanduta@gmail.com>
2025-02-08 21:41:25 -0600

    2.0

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 const Home = () => {
+    const [users, setUsers] = useState([]);
+
+    async function fetchUsers() {
+        const {data} = await axios.get('https://jsonplaceholder.typicode.com/users');
+        setUsers(data);
+    }
     return (
-        <div>
-            Home
-        </div>
+        <div className="container">
+  <div className="row">
+    <div className="user-list">
+        {users.map((user) => (
+         <div className="user">
+         <div className="user-card">
+           <div className="user-card__container">
+             <h3>{user.name}</h3>
+             <p>
+               <b>Email:</b> {user.email}
+             </p>
+             <p>
+               <b>Phone:</b> {user.phone}
+             </p>
+             <p>
+               <b>Website:</b>
+               {user.website}
+             </p>
+           </div>
+         </div>
+       </div>
+        ))}
+    </div>
+  </div>
+</div>
     )
 }
 
